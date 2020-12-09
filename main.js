9a0ecceeb09ff93e6d80e24277238a3ad8183b73
TomLynx3 <tomlynx3@gmail.com>
2020-12-09 10:46:38 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$('#btn').click(function(){
+    $.ajax({
+        url:'http://lar.daugavpils.rtu.lv/echo1.php',
+        method:"GET",
+        success:function(data){
+           $('#text').html(data)
+           $('#changeColor').css('display',"inline-block")
+        },
+        error:function (request, status, error) {
+            alert(error);
+        }
+    })
+})
+
+
+$('#changeColor').click(function(){
+    $("#text").css("color","red")
+    $(this).html("Refresh")
+    $(this).click(function(){
+        location.reload()
+    })
+})
